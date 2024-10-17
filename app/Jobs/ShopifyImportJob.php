4ded8d060794b27b3130c0afa5ebccb7574a3c73
rchitector <r.chitector@gmail.com>
2024-10-17 20:09:59 +0300

    refactoring

--- app/Jobs/ShopifyImportJob.php
+++ app/Jobs/ShopifyImportJob.php
@@ -7,7 +7,8 @@ use App\Models\Order;
 use App\Services\ShopifyService;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Facades\DB;
+
+// use Illuminate\Support\Facades\DB;
 
 class ShopifyImportJob implements ShouldQueue
 {
@@ -15,10 +16,10 @@ class ShopifyImportJob implements ShouldQueue
 
     public function handle(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;'); // For MySQL only
         Order::truncate();
         Customer::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;'); // For MySQL only
 
         ShopifyService::importCustomers();
         ShopifyService::importOrders();
